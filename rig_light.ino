797209ad51ca3541974d173424d041249eba9fa7
Joshua Salisbury <joshua.l.salisbury@gmail.com>
2024-09-24 21:47:41 -0500

    Adding strobe stuff

--- rig_light.ino
+++ rig_light.ino
@@ -1,6 +1,6 @@
 #include "FastLED.h"
 
-#define BRIGHTNESS 255
+#define BRIGHTNESS 100
 #define N_LEDS 240
 
 #define ENCODER_OUT_A 6 // CLK
@@ -10,11 +10,12 @@
 
 #define STEP 4
 
+#define STROBE_NOISE_N 149
+
 enum MODE {
   SOLID,
   SCROLL,
   STROBE,
-  FLICKER
 };
 
 enum KNOB_CHG {
@@ -25,7 +26,14 @@ enum KNOB_CHG {
   SCALE
 };
 
-MODE mode = FLICKER;
+enum STROBE_PHASE {
+  C1,
+  C12,
+  C2,
+  C21
+};
+
+MODE mode = SOLID;
 KNOB_CHG knob_chg = VAL;
 
 CRGB leds[N_LEDS];
@@ -48,19 +56,36 @@ float scroll_speed = pow(scroll_speed_x, 3) * 0.2;
 float scroll_scale = pow(1.1, 23);
 float scroll_leds_scale = 255 / N_LEDS;
 
-float strobe_speed = 0.002;
-float strobe_acc = 0;
-boolean strobe_on = false;
-
-#define FLICKER_XN 8
-float flicker_xm[FLICKER_XN];
+uint16_t strobe_t_1 = 100;
+uint16_t strobe_t_12 = 50;
+uint8_t strobe_t_12_8frac = 255 / strobe_t_12;
+uint16_t strobe_t_2 = 200;
+uint16_t strobe_t_21 = 25;
+uint8_t strobe_t_21_8frac = 255 / strobe_t_21;
+uint8_t strobe_t_frac;
+uint8_t strobe_fade_frac;
+float strobe_c1_hue = 255;
+float strobe_c1_sat = 0;
+float strobe_c1_val = 255;
+float strobe_c2_hue = 180;
+float strobe_c2_sat = 255;
+float strobe_c2_val = 0;
+unsigned long strobe_acc = 0;
+float strobe_fade = 0;
+float strobe_phase_start_t;
+STROBE_PHASE strobe_phase = C1;
+uint8_t strobe_noise[STROBE_NOISE_N];
+uint8_t strobe_noise_idx = 0;
+uint8_t get_strobe_noise() {
+  while (strobe_noise_idx >= STROBE_NOISE_N) {
+    strobe_noise_idx -= STROBE_NOISE_N;
+  }
+  return strobe_noise[strobe_noise_idx++];
+}
 
 void setup() {
   delay(1000);
   random16_set_seed(analogRead(0));
-  for (uint8_t i = 0; i < FLICKER_XN; i++) {
-    flicker_xm[i] = random8();
-  }
 
   FastLED.addLeds<NEOPIXEL, LED_OUT>(leds, N_LEDS);
   FastLED.setBrightness(BRIGHTNESS);
@@ -79,6 +104,10 @@ void setup() {
 
   t_m = millis();
   old_t_m = t_m;
+
+  for (uint8_t i = 0; i < STROBE_NOISE_N; i++) {
+    strobe_noise[i] = random8();
+  }
 }
 
 void loop_solid() {
@@ -236,8 +265,73 @@ void loop_scroll() {
 
 void loop_strobe() {
   t_m = millis();
-  strobe_acc += (t_m - old_t_m) * strobe_speed;
+  strobe_acc += t_m - old_t_m;
   old_t_m = t_m;
+  uint8_t fade_floor = 3;
+
+  switch (strobe_phase) {
+    case C1:
+      for (uint8_t i = 0; i < N_LEDS; i++) {
+        uint8_t j = get_strobe_noise();
+        if (j >= 3) { // 13 ~= 128 / 10
+          leds[i] = CHSV(strobe_c1_hue, strobe_c1_sat, strobe_c1_val);
+        } else {
+          leds[i] = CHSV(strobe_c2_hue, strobe_c2_sat, strobe_c2_val);
+        }
+      }
+      if (strobe_acc >= strobe_t_1) {
+        strobe_acc -= strobe_t_1;
+        strobe_phase = C12;
+      }
+      break;
+    case C12:
+      strobe_t_frac = strobe_acc * 255 / strobe_t_12;
+      strobe_fade_frac = blend8(fade_floor, 255 - fade_floor, strobe_t_frac);
+      for (uint8_t i = 0; i < N_LEDS; i++) {
+        uint8_t j = get_strobe_noise();
+        if (j >= strobe_fade_frac) {
+          leds[i] = CHSV(strobe_c1_hue, strobe_c1_sat, strobe_c1_val);
+        } else {
+          leds[i] = CHSV(strobe_c2_hue, strobe_c2_sat, strobe_c2_val);
+        }
+      }
+      if (strobe_acc >= strobe_t_12) {
+        strobe_acc -= strobe_t_12;
+        strobe_phase = C2;
+      }
+      break;
+    case C2:
+      for (uint8_t i = 0; i < N_LEDS; i++) {
+        uint8_t j = get_strobe_noise();
+        if (j >= fade_floor) {
+          leds[i] = CHSV(strobe_c2_hue, strobe_c2_sat, strobe_c2_val);
+        } else {
+          leds[i] = CHSV(strobe_c1_hue, strobe_c1_sat, strobe_c1_val);
+        }
+      }
+      if (strobe_acc >= strobe_t_2) {
+        strobe_acc -= strobe_t_2;
+        strobe_phase = C21;
+      }
+      break;
+    case C21:
+      strobe_t_frac = strobe_acc * 255 / strobe_t_21;
+      strobe_fade_frac = blend8(255 - fade_floor, fade_floor, strobe_t_frac);
+      for (uint8_t i = 0; i < N_LEDS; i++) {
+        uint8_t j = get_strobe_noise();
+        if (j >= strobe_fade_frac) {
+          leds[i] = CHSV(strobe_c1_hue, strobe_c1_sat, strobe_c1_val);
+        } else {
+          leds[i] = CHSV(strobe_c2_hue, strobe_c2_sat, strobe_c2_val);
+        }
+      }
+      if (strobe_acc >= strobe_t_21) {
+        strobe_acc -= strobe_t_21;
+        strobe_phase = C1;
+      }
+      break;
+  }
+  FastLED.show();
 
   if (state_c == 0) {
     if (!clk) {
@@ -254,36 +348,14 @@ void loop_strobe() {
         old_t_m = t_m = millis();
         return;
       }
-      strobe_speed /= 1.1;
     } else {
       if (clk) {
         mode = SOLID;
         return;
       }
-      strobe_speed *= 1.1;
     }
   }
-
-  if (strobe_on) {
-    strobe_on = false;
-    for (uint8_t i = 0; i < N_LEDS; i++) {
-      leds[i] = CRGB::Black;
-    }
-    FastLED.show();
-  }
-
-  if (strobe_acc >= 1) {
-    strobe_on = true;
-    strobe_acc = 0;
-    for (uint8_t i = 0; i < N_LEDS; i++) {
-      leds[i] = CHSV(hue, sat, val);
-    }
-    FastLED.show();
-  }
-}
-
-void loop_flicker() {
-  t_m = millis();
+  delay(10);
 }
 
 void loop() {
@@ -298,8 +370,6 @@ void loop() {
     loop_scroll();
   } else if (mode == STROBE) {
     loop_strobe();
-  } else if (mode == FLICKER) {
-    loop_flicker();
   }
   oldstate_a = state_a;
 }
